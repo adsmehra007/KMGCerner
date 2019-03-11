408a67aa65e8df6de662b855465fe9f50f055346
AkashDeep Singh Mehra <as.mehra007@gmail.com>
2019-03-11 17:39:42 +0530

    Update example-smart-app.js

--- src/js/example-smart-app.js
+++ src/js/example-smart-app.js
@@ -40,6 +40,7 @@
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var patientAddress =patient.address;
+          console.log('Add',patientAddress);
  console.log('Address',patientAddress.text+" "+patientAddress.city+" "+patientAddress.district);
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
