1288b77393563654e156fff6c00267612c79727e
AkashDeep Singh Mehra <as.mehra007@gmail.com>
2019-03-12 19:00:12 +0530

    Update example-smart-app.js

--- src/js/example-smart-app.js
+++ src/js/example-smart-app.js
@@ -45,7 +45,7 @@
     return false;
   });
 });
-             smart.patient.api.fetchAll({type:"MedicationOrder"}).then(function(results, refs) {
+             smart.patient.api.fetchAll({type:"Medications"}).then(function(results, refs) {
   results.forEach(function(medOrder){
      console.log('Medication Order : ',medOrder);
     return false;
